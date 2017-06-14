d95a50a1ac3b8ae9abe63aae62b49e07006bcedd
TaraLoveCats <280895202@qq.com>
2017-06-14 11:13:19 +0800

    update

--- task0002/js/task0002_5.js
+++ task0002/js/task0002_5.js
@@ -1,56 +1,45 @@
-function dragDrop() {
-    var dragging = null,
-        diffX = 0,
-        diffY = 0;
+var dragging = null,
+    diffX = 0,
+    diffY = 0;
 
-    function handleEvent(event) {
-        event = event || window.event;
-        var target = event.target || event.srcElement;
+function handleMouseDown(e) {
+    event = e || window.event;
+    var target = event.target || event.srcElement;
 
-        switch(event.type) {
-            case 'mousedown':
-                if (target.className.indexOf('draggable') > -1) {
-                    dragging = target;
-                    diffX = event.clientX - target.offsetLeft;
-                    diffY = event.clientY - target.offsetTop;
-                }
-                break;
-
-            case 'mousemove':
-                if (dragging !== null) {
-                    dragging.style.left = (event.clientX - diffX) + 'px';
-                    dragging.style.top = (event.clientY - diffY) + 'px';
-                }
-                break;
-
-            case 'mouseup':
-                dragging = null;
-                break;
-        }
-    }
-
-    return {
-        enable: function () {
-            addEvent($('.left-box'), 'mousedown', handleEvent);
-            addEvent($('.left-box'), 'mousemove', handleEvent);
-            addEvent($('.left-box'), 'mouseup', handleEvent);
-
-            addEvent($('.right-box'), 'mousedown', handleEvent);
-            addEvent($('.right-box'), 'mousemove', handleEvent);
-            addEvent($('.right-box'), 'mouseup', handleEvent);
-        },
-
-        disable: function () {
-            removeEvent($('.left-box'), 'mousedown', handleEvent);
-            removeEvent($('.left-box'), 'mousemove', handleEvent);
-            removeEvent($('.left-box'), 'mouseup', handleEvent);
-
-            removeEvent($('.right-box'), 'mousedown', handleEvent);
-            removeEvent($('.right-box'), 'mousemove', handleEvent);
-            removeEvent($('.right-box'), 'mouseup', handleEvent);
-        }
+    if (target.className.indexOf('draggable') > -1) {
+        dragging = target;
+        diffX = event.clientX - target.offsetLeft;
+        diffY = event.clientY - target.offsetTop;
     }
 }
 
-var result = dragDrop();
-result.enable();
+function handleMouseMove(e) {
+    event = e || window.event;
+    var target = event.target || event.srcElement;
+
+    if (dragging !== null) {
+        dragging.style.left = (event.clientX - diffX) + 'px';
+        dragging.style.top = (event.clientY - diffY) + 'px';
+    }
+}
+
+function handleMouseUp(e) {
+    event = e || window.event;
+    var target = event.target || event.srcElement;
+
+    dragging = null;
+}
+
+function enable() {
+    addEvent(document, 'mousedown', handleMouseDown);
+    addEvent(document, 'mousemove', handleMouseMove);
+    addEvent(document, 'mouseup', handleMouseUp);
+}
+
+function disable() {
+    removeEvent(document, 'mousedown', handleMouseDown);
+    removeEvent(document, 'mousemove', handleMouseMove);
+    removeEvent(document, 'mouseup', handleMouseUp);
+}
+
+enable();
