a804b27e2a0ad056a75400d8a7d8cf1adb16c4fc
TaraLoveCats <280895202@qq.com>
2017-06-14 21:53:05 +0800

    update

--- task0002/js/task0002_5.js
+++ task0002/js/task0002_5.js
@@ -8,15 +8,13 @@ function handleMouseDown(e) {
 
     if (target.className.indexOf('draggable') > -1) {
         dragging = target;
+        dragging.style.opacity = 0.7;
         diffX = event.clientX - target.offsetLeft;
         diffY = event.clientY - target.offsetTop;
     }
 }
 
 function handleMouseMove(e) {
-    event = e || window.event;
-    var target = event.target || event.srcElement;
-
     if (dragging !== null) {
         dragging.style.left = (event.clientX - diffX) + 'px';
         dragging.style.top = (event.clientY - diffY) + 'px';
@@ -24,9 +22,7 @@ function handleMouseMove(e) {
 }
 
 function handleMouseUp(e) {
-    event = e || window.event;
-    var target = event.target || event.srcElement;
-
+    dragging.style.opacity = 1;
     dragging = null;
 }
 
@@ -43,3 +39,5 @@ function disable() {
 }
 
 enable();
+console.log('rightbox: ' + $('.right-box').offsetLeft);
+console.log('leftbox: ' + $('.left-box').offsetLeft);
